1bcf6e24ba0968348511498fd23a9bb481c3305a
Palak <palak7372@gmail.com>
2020-01-23 18:49:47 +0530

    fixed some bugs

--- models/index.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose');
-// mongoose.set('debug', true);
-// mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
-
-// mongoose.Promise = Promise;
-
-// module.exports.Todo = require("./todo");
-// module.exports.User = require("./user");
